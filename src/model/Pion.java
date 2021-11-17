d1d7ef5c73ca4584708912996727e1f412cae12e
H4CK3R5 <75364799+H4CK3R5-FS@users.noreply.github.com>
2021-11-17 19:25:48 +0100

    Update Pion.java

--- src/model/Pion.java
+++ src/model/Pion.java
@@ -1,40 +1,37 @@
 package model;
 
 public class Pion {
-String couleur;
-int id,y,x;
+	String couleur;
+	int id,y,x;
 
-public Pion(String couleur, int id, int y, int x) {
-	this.couleur = couleur;
-	this.id = id;
-	this.y = y;
-	this.x = x;
-}
-public String getCouleur() {
-	return couleur;
-}
-public void setCouleur(String couleur) {
-	this.couleur = couleur;
-}
-public int getId() {
-	return id;
-}
-public void setId(int id) {
-	this.id = id;
-}
-public int getY() {
-	return y;
-}
-public void setY(int y) {
-	this.y = y;
-}
-public int getX() {
-	return x;
-}
-public void setX(int x) {
-	this.x = x;
-}
-
-
-
+	public Pion(String couleur, int id, int y, int x) {
+		this.couleur = couleur;
+		this.id = id;
+		this.y = y;
+		this.x = x;
+	}
+	public String getCouleur() {
+		return couleur;
+	}
+	public void setCouleur(String couleur) {
+		this.couleur = couleur;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
 }
