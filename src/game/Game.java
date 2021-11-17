8db46b82e9fcf85ab161be808031f24378bc2313
H4CK3R5 <75364799+H4CK3R5-FS@users.noreply.github.com>
2021-11-17 19:24:25 +0100

    Update Game.java

--- src/game/Game.java
+++ src/game/Game.java
@@ -11,11 +11,11 @@ public class Game {
 
 	String [][] board=new String[10][10];
 	ArrayList<Pion> alP=new ArrayList<Pion>();
-public void game() {
-	Utilitaires.draw_Board(board);
+	public void game() {
+		Utilitaires.draw_Board(board);
 
-	Utilitaires.add_Pieces_To_List(alP);
-	Utilitaires.add_Pieces_To_Board(alP, board);
-	Utilitaires.read_Board(board);
-}
+		Utilitaires.add_Pieces_To_List(alP);
+		Utilitaires.add_Pieces_To_Board(alP, board);
+		Utilitaires.read_Board(board);
+	}
 }
